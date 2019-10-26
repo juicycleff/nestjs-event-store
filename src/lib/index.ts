b17ee947fddb02209666dec59297abe6971dfb35
Rex Isaac Raphael <rex.raphael@outlook.com>
2019-10-26 21:51:26 +0200

    updated readme.me

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,2 +1,5 @@
 export * from './nestjs-event-store.module';
 export * from './nestjs-event-store.service';
+export * from './event-store';
+export * from './nestjs-event-store.constant';
+export * from './event-store-option.config';
